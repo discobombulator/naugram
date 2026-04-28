6713a2b5c58df12d9281b54432dd7c087ee94ebf
Ivan <kuuuuushtanov@gmail.com>
2026-04-29 00:36:23 +0500

    Init

---
+++ src/main/java/something/ru/NauGram/NauGramApplication.java
@@ -0,0 +1,13 @@
+package something.ru.NauGram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NauGramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/something/ru/NauGram/NauGramApplicationTests.java
@@ -0,0 +1,13 @@
+package something.ru.NauGram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauGramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
