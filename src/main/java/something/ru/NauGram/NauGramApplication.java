d695c3a4d2187eec5588d111e905635671119f1a
Ozornin Yaroslav <Runboll023@gmail.com>
2026-05-03 14:35:01 +0500

    User identification + auth + 2FA + sessions + notifications

--- src/main/java/something/ru/NauGram/NauGramApplication.java
+++ src/main/java/something/ru/NauGram/NauGramApplication.java
@@ -2,8 +2,12 @@ package something.ru.NauGram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class NauGramApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/something/ru/NauGram/model/VerificationToken.java
@@ -0,0 +1,57 @@
+package something.ru.NauGram.model;
+
+import jakarta.persistence.*;
+import something.ru.NauGram.model.User;
+
+import java.security.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Entity
+public class VerificationToken {
+    public static final int EXPIRATION = 15;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private Date expiryDate;
+
+    public String getToken() {
+        return token;
+    }
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+    public User getUser() {
+        return user;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public Date calculateExpiryDate(int expiryTimeInMinutes) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return cal.getTime();
+    }
+
+}
